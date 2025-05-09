b0babd6c4483b587353c017f7a42597118f2ff6a
Remy-cloud <r.intwali@alustudent.com>
2025-05-09 19:44:07 +0200

    updated the file

--- models/base_model.py
+++ models/base_model.py
@@ -7,29 +7,28 @@ from datetime import datetime
 
 class BaseModel:
     """
-    base class for all airbnb clone models with 
-    attributes 
+    Base class for all AirBnB clone models with attributes and methods.
     """
 
-    def __init__(self, *args , **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Initialize a new BaseModel instance.
 
         If kwargs is provided, it recreates an instance from a dictionary.
         Otherwise, it creates a new instance with a unique ID and timestamps.
         """
-
         if kwargs:
             for key, value in kwargs.items():
-                if key =="created_at" or key == "updated_at":
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
                     value = datetime.fromisoformat(value)
-                    if key != "__class__":
-                        setattr(self, key, value)
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-    
+
     def __str__(self):
         """
         Return string representation of the BaseModel instance.
@@ -39,15 +38,14 @@ class BaseModel:
 
     def save(self):
         """
-        updated the "update_at" attribute with the current time and date
+        Update the 'updated_at' attribute with the current time.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        returns a dictionary representation of the instance
+        Return a dictionary representation of the instance.
         """
-
         dict_rep = self.__dict__.copy()
         dict_rep["__class__"] = self.__class__.__name__
         dict_rep["created_at"] = self.created_at.isoformat()
