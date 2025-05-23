9b264f731273e30a0cc77869c014dfc8eff97c06
Remy-cloud <r.intwali@alustudent.com>
2025-05-23 18:37:53 +0200

    PEP8 validation

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage  #Import storage instance
+from models import storage
 
 
 class BaseModel:
@@ -30,7 +30,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)  #Register new object in storage
+            storage.new(self)
 
     def __str__(self):
         """
@@ -46,7 +46,7 @@ class BaseModel:
         """
         self.updated_at = datetime.now()
         from models import storage
-        storage.save()  #Persist storage to file
+        storage.save()
 
     def to_dict(self):
         """
