1782f5ad1d3a33d3701d634c4e30d7de91ffc61e
Remy-cloud <r.intwali@alustudent.com>
2025-05-23 17:56:05 +0200

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,39 +1,52 @@
-#!/usr/bin/env python3
-"""FileStorage module: serializes and deserializes BaseModel instances to/from JSON file"""
-
 import json
-from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Handles serialization and deserialization of BaseModel instances"""
-
     __file_path = "file.json"
     __objects = {}
 
+    def clear(self):
+        self.__objects = {}
+        with open(self.__file_path, "w") as f:
+            json.dump({}, f)
+
     def all(self):
-        """Returns the dictionary of all stored objects"""
         return self.__objects
 
     def new(self, obj):
-        """Adds a new object to storage dictionary"""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file"""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, "w") as f:
-            obj_dict = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file back to __objects"""
+        """Deserializes the JSON file to __objects"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+
+        classes = {
+            'BaseModel': BaseModel,
+            'User': User,
+            'Amenity': Amenity,
+            'City': City,
+            'Place': Place,
+            'Review': Review,
+            'State': State,
+        }
+
         try:
             with open(self.__file_path, "r") as f:
                 obj_dict = json.load(f)
-                for key, val in obj_dict.items():
-                    class_name = val["__class__"]
-                    if class_name == "BaseModel":
-                        self.__objects[key] = BaseModel(**val)
+                for key, value in obj_dict.items():
+                    if value['__class__'] in classes:
+                        self.__objects[key] = classes[value['__class__']](
+                            **value)
         except FileNotFoundError:
             pass
